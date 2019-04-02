70a123a459e5d8f38e47dc240d3b52caf3d56bf9
Albert Garcia-Garcia <agarcia@dtic.ua.es>
2019-04-02 20:32:02 +0200

    F2: P1: completados la mayoría de los cálculos y gráficas de freq. relativista

--- fisica_2/practica_1/frec_relativista_g_all.py
+++ fisica_2/practica_1/frec_relativista_g_all.py
@@ -26,20 +26,13 @@ q=1.6e-19   # Carga del ion
 B=1.0  # Campo magnético
 #Ecr=(np.random.rand())*40+10
 #Ec=round(Ecr,2)
-vx0=1.0e8  # velocidad del ion
+vx0=[0.2e8, 0.6e8, 0.8e8, 1.0e8]  # velocidad del ion
 tf=12e-8  #tiempo de simulacion. Debe ser 2 o 3 veces superior al periodo del ion
 # para poder ver varios ciclos
 
 
 c=3e8  #velocidad de la luz en el vacio
-g=np.sqrt(1-((vx0/c)*(vx0/c))) #factor gamma
-mr=m*g  # si se quiere estudiar el caso relativista usar mr=m*g si no mr=m 
-#mr=m
 
-
-
-qm=q/mr
-par=[qm,B]
 
 
 # Definicion de las ecuaciones de movimiento
@@ -53,42 +46,50 @@ def circulo(z,t,par):
 # Definicion del grafico
 # Modificar los limites de acuerdo a las necesidades, así como los titulos de los ejes
 
-matplotlib.rc('xtick', labelsize=32) 
-matplotlib.rc('ytick', labelsize=32) 
+matplotlib.rc('xtick', labelsize=24) 
+matplotlib.rc('ytick', labelsize=24) 
 plt.rc('text', usetex=False)
 plt.rc('font', family='serif')
-plt.rc('font', size='36')
+plt.rc('font', size='32')
 
 ax=plt.axes(xlim=(0,tf))
 
 
 # Llamada a odeint que resuleve las ecuaciones de movimiento
 
-nt=10000
-z0=[-0.1,vx0,0.,0.0]    
-t=np.linspace(0,tf,nt)
-abserr = 1.0e-8
-relerr = 1.0e-6
-z=odeint(circulo,z0,t,args=(par,),atol=abserr, rtol=relerr)
+for v in range(len(vx0)):
 
-min_ = 0.0
-last_t_ = 0.0
+    g=np.sqrt(1-((vx0[v]/c)*(vx0[v]/c))) #factor gamma
+    mr=m*g  # si se quiere estudiar el caso relativista usar mr=m*g si no mr=m 
+    #mr=m
+    qm=q/mr
+    par=[qm,B]
 
-for i in range(len(t[:])):
+    nt=10000
+    z0=[-0.1,vx0[v],0.,0.0]    
+    t=np.linspace(0,tf,nt)
+    abserr = 1.0e-8
+    relerr = 1.0e-6
+    z=odeint(circulo,z0,t,args=(par,),atol=abserr, rtol=relerr)
 
-    if (abs(z[i, 2] - min_) < 1e-12):
-        print("Encontrado siguiente mínimo")
-        print("min = " + str(min_))
-        print("z = " + str(z[i, 2]))
-        print("En t = " + str(t[i]))
+    min_ = 0.0
+    last_t_ = 0.0
 
-    if (z[i, 2] < min_):
-        min_ = z[i, 2]
-        last_t_ = t[i]
+    for i in range(len(t[:])):
 
-line1, = ax.plot(t[:],z[:,2],'-', label=r"v = " + str(vx0/1e8) + r" $\cdot 10^8 [ms^{-1}]$", linewidth=2)
+        if (abs(z[i, 2] - min_) < 1e-12):
+            print("Encontrado siguiente mínimo")
+            print("min = " + str(min_))
+            print("z = " + str(z[i, 2]))
+            print("En t = " + str(t[i]))
 
-ax.legend(loc='upper right')
+        if (z[i, 2] < min_):
+            min_ = z[i, 2]
+            last_t_ = t[i]
+
+    line1, = ax.plot(t[:],z[:,2],'-', label=r"v = " + str(vx0[v]/1e8) + r" $\cdot 10^8 [ms^{-1}]$", linewidth=2)
+
+ax.legend(loc='lower center')
 
 ax.ticklabel_format(axis='y', style='sci', scilimits=(-2,2))
 
