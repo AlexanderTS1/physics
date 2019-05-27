9bae6d924ff0cf1f9b8206424e57f00c19a13039
Albert Garcia <agarcia@dtic.ua.es>
2019-05-27 15:07:15 +0200

    F2: P2: introduccion y marco teorico RCL con generador

--- fisica_2/practica_2/RCL_congen.py
+++ fisica_2/practica_2/RCL_congen.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import argparse
 import numpy as np
 import math
 import random
@@ -9,75 +10,84 @@ from scipy.integrate import odeint
 #import matplotlib.animation as animation
 
 
-fig=plt.figure()
-fig.set_dpi(100)
-#fig.set_size_inches(7,6.5)
+def simular(args):
+    fig=plt.figure()
+    fig.set_dpi(100)
+    #fig.set_size_inches(7,6.5)
 
 
-# Datos a modificar 
-# Circuito RLC en serie con un generador AC cuyo voltaje es V0*cos(omega*t)
+    # Datos a modificar 
+    # Circuito RLC en serie con un generador AC cuyo voltaje es V0*cos(omega*t)
 
-R=100.0  #resistencia
-C=1.E-5  #Capacidad del condensador
-L=0.1    #Inductancia de la bobina
+    R=args.R  #resistencia
+    C=args.C  #Capacidad del condensador
+    L=args.L    #Inductancia de la bobina
 
-f=50.0   #frecuencia del generador
-Veficaz=220.0 #voltaje eficaz del generador
-V0=Veficaz*np.sqrt(2.0) #voltaje máximo del generador
-omega=2.*math.pi*f  # frecuencia angular
-tf=5.E-1  # tiempo de la simulacion
+    f=args.f   #frecuencia del generador
+    Veficaz=args.V #voltaje eficaz del generador
+    V0=Veficaz*np.sqrt(2.0) #voltaje máximo del generador
+    omega=2.*math.pi*f  # frecuencia angular
+    tf=args.tf  # tiempo de la simulacion
 
-par=[R,C,L]
-
-
-
-# Definicion de las ecuaciones de movimiento
-def circRCL(z,t,par):
-    q,qp=z  
-    dzdt=[qp,-q/C/L-R*qp/L+V0*math.cos(omega*t)/L]
-    return dzdt
-
-
-# Llamada a odeint que resuelve las ecuaciones de movimiento
-
-nt=10000  #numero de intervalos de tiempo
-z0=[0.0,0.0] #Valores iniciales de Q e I    
-t=np.linspace(0,tf,nt)
-abserr = 1.0e-8
-relerr = 1.0e-6
-z=odeint(circRCL,z0,t,args=(par,),atol=abserr, rtol=relerr)
-
-
-plt.close('all')
-
-# Definicion del grafico
-# Realiza una gráfica del voltaje del generador y de la corriente del circuito
-# en función del tiempo. Cada curva tiene un eje y diferente
-
-fig, ax1 = plt.subplots()
-
-ax1.set_xlabel('time (s)')
-
-ax1.set_ylabel('I (A)', color='b')
-ax1.tick_params('y', colors='b')
-
-ax2 = ax1.twinx()
-
-ax1.set_xlim(xmin=0.08,xmax=.16) #limites del eje x
-
-
-line1, = ax1.plot(t[:],z[:,1],'--', linewidth=2, color='b')
-line2, = ax2.plot(t[:],V0*np.cos(omega*t[:]),'--', linewidth=2, color='r')
-
-
-ax2.set_ylabel('Vgen (V)', color='r')
-ax2.tick_params('y', colors='r')
-
-fig.tight_layout()
-
-plt.show()
+    par=[R,C,L]
 
 
 
+    # Definicion de las ecuaciones de movimiento
+    def circRCL(z,t,par):
+        q,qp=z  
+        dzdt=[qp,-q/C/L-R*qp/L+V0*math.cos(omega*t)/L]
+        return dzdt
 
 
+    # Llamada a odeint que resuelve las ecuaciones de movimiento
+
+    nt=10000  #numero de intervalos de tiempo
+    z0=[0.0,0.0] #Valores iniciales de Q e I    
+    t=np.linspace(0,tf,nt)
+    abserr = 1.0e-8
+    relerr = 1.0e-6
+    z=odeint(circRCL,z0,t,args=(par,),atol=abserr, rtol=relerr)
+
+
+    plt.close('all')
+
+    # Definicion del grafico
+    # Realiza una gráfica del voltaje del generador y de la corriente del circuito
+    # en función del tiempo. Cada curva tiene un eje y diferente
+
+    fig, ax1 = plt.subplots()
+
+    ax1.set_xlabel('time (s)')
+
+    ax1.set_ylabel('I (A)', color='b')
+    ax1.tick_params('y', colors='b')
+
+    ax2 = ax1.twinx()
+
+    ax1.set_xlim(xmin=0.08,xmax=.16) #limites del eje x
+
+
+    line1, = ax1.plot(t[:],z[:,1],'--', linewidth=2, color='b')
+    line2, = ax2.plot(t[:],V0*np.cos(omega*t[:]),'--', linewidth=2, color='r')
+
+    ax2.set_ylabel('Vgen (V)', color='r')
+    ax2.tick_params('y', colors='r')
+
+    fig.tight_layout()
+
+    plt.show()
+
+if __name__ == "__main__":
+
+    parser_ = argparse.ArgumentParser(description="Parameters")
+    parser_.add_argument("--C", nargs="?", type=float, default=75e-6, help="C")
+    parser_.add_argument("--L", nargs="?", type=float, default=0.30, help="L")
+    parser_.add_argument("--R", nargs="?", type=float, default=300, help="R")
+    parser_.add_argument("--f", nargs="?", type=float, default=75, help="f")
+    parser_.add_argument("--V", nargs="?", type=float, default=165, help="V")
+    parser_.add_argument("--tf", nargs="?", type=float, default=1.0e-1, help="tf")
+
+    args_ = parser_.parse_args()
+
+    simular(args_)
